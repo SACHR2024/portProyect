a8ff10a378f70b7d6c3e16fcdea619c04c97bd9b
Samuel Chirinos <samuelchirinos71@gmail.com>
2024-08-17 16:17:56 -0500

    ✨ feat(angularMaterialModule and angularAnimations are enable): routingModules edt
    routingModules  configured, welcome route added. Angular material join to the proyect

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -12,6 +12,10 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NavBarComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
